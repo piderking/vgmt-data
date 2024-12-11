4626b4a97c17c3ec6af305bdacc9002c88b6bf9c
piderking <piderking8@gmail.com>
2024-12-10 20:34:39 -0800

    introduced an overwrite of the threading system and added the algorithmic checking of the to save into the saving

--- server/data/acceptor/sorter.py
+++ server/data/acceptor/sorter.py
@@ -9,6 +9,8 @@ import time
 from ...worker.endpoint import users
 from .datapool import DataPool
 import random
+from .table import TableResponse
+from ..saving.save import save_table
 
 
 type SortedStore = dict[UID, list[SortedDataModel]]
@@ -19,7 +21,11 @@ class DataAcceptorSorter(threading.Thread):
     pool: DataPool[list[SortedDataModel]] 
     _sorted: SortedStore
     overflow: int = 3
-
+    
+    
+    
+    metadata: dict[UID, dict[str, int | str]] = {}
+    tables: dict[UID, TableResponse] = {}
     
 
     def get(self, key: UID = None, multi: bool | int = False) -> list[ list[ SortedDataModel ]] | list[ SortedDataModel ]:
@@ -34,11 +40,23 @@ class DataAcceptorSorter(threading.Thread):
         return self._sorted
     @sorted.setter
     def sorted(self, cleaned:CleanedDataModel) -> SortedStore:
-        if self._sorted.get(cleaned) is None: self._sorted[cleaned.uid] = []
+        if self._sorted.get(cleaned.uid) is None: 
+            self.metadata[cleaned.uid] = {
+                "last_used": time.time(),
+                "table_alive": False
+                # size of non tabled data
+            }
+            self.tables[cleaned.uid] = None #  TableResponse(time=cleaned.time,)
+            
+            # table.check_sourced
+            self._sorted[cleaned.uid] = []
+            
         self._sorted[cleaned.uid].append(cleaned.to_sorted())
         
-        return self._sorted
-    
+        
+        #return self._sorted
+    def take_sorted(self, uid: UID) -> list[SortedDataModel]:
+        return self._sorted.pop(uid) # pop the data
     def __init__(self, data_cleaner: DataAcceptorCleaner | None, daemon: bool = True, auto_start: bool = False, max_workers: int = 2):
         self.data_cleaner = data_cleaner or DataAcceptorCleaner(daemon=daemon, auto_start=auto_start, max_workers=max_workers)
         self.cut_size = data_cleaner.cut_size # sync cut_size
@@ -53,6 +71,45 @@ class DataAcceptorSorter(threading.Thread):
 
         if auto_start or bool(CONFIG.THREAD["auto_start"]): self.start() # s
 
+    def create_table(self, uid: str) -> TableResponse:
+        if self.sorted.get(uid) and self.metadata.get(uid):
+            if not self.metadata[uid]["table_alive"]:
+                self.tables[uid] = TableResponse(time.time()) # set time to now, new data will change the time
+            self.tables[uid] = self.tables[uid] + self.take_sorted(uid)
+            self.metadata[uid] = dict(self.metadata[uid] | {
+                "last_used":time.time(),
+                "table_alive": True
+            })
+                
+            return  self.tables[uid]
+    def save(self, uid: UID):
+        
+        
+        if self.metadata.get(uid) and self.metadata[uid]["table_alive"]:
+            self.create_table(uid)
+        
+            self.tables[uid] = self.tables[uid].check_sources(uid)
+            
+            tbl = self.tables.pop(uid)
+            save_table(tbl, uid, tbl.time, overwrite=True) # check_sources is run before so we can run itr
+
+            self.tables[uid] = None
+
+            self.metadata[uid] = dict(self.metadata[uid] | {
+                "last_used":time.time(),
+                "table_alive": False
+            })
+        
+        
+    def join(self, t: int = None) -> None:
+        while len(self.data_cleaner._final ) > 0 or len(self.data_cleaner.responses) > 0: # Check Data Cleaner object to be empty
+            pass
+        
+        for uid in self._sorted.keys():
+            # convert to table & save
+            self.save(uid)
+        
+        super(self).join(t) 
     @property
     def data(self,):
         """
@@ -66,14 +123,21 @@ class DataAcceptorSorter(threading.Thread):
         self.sorted = data
         ...
         
-    def process(self, executor: ThreadPoolExecutor, batch: list[CleanedDataModel]) -> None:
-        self._final.extend(executor.map(self.sort, *[batch]))         
+    
+    def check_user(self, uid: UID):
+        if self.sorted.get(uid):
+            ...
+        else:
+            return False
+    
+    def process(self, executor: ThreadPoolExecutor, batch: list[CleanedDataModel], func) -> None:
+        self._final.extend(executor.map(func, *[batch]))         
     
     def execute(self, executor: ThreadPoolExecutor):
         if len(self.data_cleaner._final) > 0:
     
             #taking = len(self.responses) - self.cut_size if len(self.responses) > self.cut_size else 0
-            self.process(executor, self.data_cleaner.final)
+            self.process(executor, self.data_cleaner.final, self.sort)
             return True
         else: # skip this itteration
             return False
@@ -81,9 +145,15 @@ class DataAcceptorSorter(threading.Thread):
         info("Starting Sorting Server for Datapool")
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             while self.is_alive() and executor:
-                if self.data_cleaner.execute(executor) or self.execute(executor):
+                if self.data_cleaner.execute(executor) and self.execute(executor):
                     ...
                 else:
+                    # place holder values
+                    MAXIMUM_ITEMS = 5
+                    self.process(executor, [k for k, i in self.sorted.items() if len(list(i)) >= MAXIMUM_ITEMS], self.save)
+
+                    # isn't needed -- process service threads
+                    
                     # Isn't needed and will stop boggling the system and sleep for 1/4 of second
                     time.sleep(CONFIG.THREAD["time_out"]) # TODO add customizability to timeing in confi
         info("Thread Killed", _type="DataAcceptor", name="Cleaner")
